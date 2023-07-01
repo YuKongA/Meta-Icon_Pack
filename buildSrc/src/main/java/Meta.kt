444d6d473bca852e179d9a59f253fed869bb0ff2
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-07-01 15:33:10 +0800

    icon: Sync to latest icon version

--- buildSrc/src/main/java/Meta.kt
+++ buildSrc/src/main/java/Meta.kt
@@ -2,6 +2,6 @@
 
 object Meta {
     const val appId = "com.yuk.meta"
-    const val version = 106
-    const val versionName = "1.0.6"
+    const val version = 107
+    const val versionName = "1.0.7"
 }
